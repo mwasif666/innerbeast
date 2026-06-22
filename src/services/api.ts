4c81c417ec7a446e62fecbc66cca1b1a56537883
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-06-22 19:04:12 +0500

    Implement API wrapper

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,12 @@
-export {}
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
+const api = async (endpoint: string) => {
+  if (!BASE_URL) {
+    throw new Error('NEXT_PUBLIC_API_URL is missing')
+  }
+
+  const response = await fetch(BASE_URL + endpoint)
+  return response.json()
+}
+
+export default api
