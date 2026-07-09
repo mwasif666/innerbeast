aa6466396a88db9c27374c83c17423bef48855ad
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-07-09 19:34:39 +0500

    Mount support widget on help page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -4,6 +4,7 @@ import TopNavOne from '@/components/Header/TopNav/TopNavOne'
 import MenuOne from '@/components/Header/Menu/MenuOne'
 import Breadcrumb from '@/components/Breadcrumb/Breadcrumb'
 import Footer from '@/components/Footer/Footer'
+import SupportWidget from '@/components/Support/SupportWidget'
 
 export default function HelpPage() {
   return (
@@ -18,6 +19,7 @@ export default function HelpPage() {
         <p className='body1 text-secondary mt-4 max-w-2xl'>Message our team about orders, delivery, products, returns, or account support.</p>
       </main>
       <Footer />
+      <SupportWidget />
     </>
   )
 }
