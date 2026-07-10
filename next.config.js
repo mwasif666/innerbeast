5983ef4d4289e4a9cad7784094ea911a2a28445d
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-07-10 16:22:00 +0500

    Disable Next image optimizer for Hostinger

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   },
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "res.cloudinary.com" },
