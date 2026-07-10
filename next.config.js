53b3cdf5430b7e892ae5e5c33f432f609059d8bb
Muhammad Wasif <mwasif66625426@gmail.com>
2026-07-10 16:19:00 +0500

    new chnages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        remotePatterns: [
-            { protocol: 'https', hostname: 'images.unsplash.com' },
-            { protocol: 'https', hostname: 'res.cloudinary.com' },
-            { protocol: 'https', hostname: 'static-01.daraz.pk' },
-        ],
-    },
-}
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "static-01.daraz.pk" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
